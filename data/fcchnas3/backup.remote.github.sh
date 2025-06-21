f77eb0907ee6dcf4193e7aa7ecd0ca3e4a9d0596
Stephen Warren <swarren@wwwdotorg.org>
2025-06-21 11:55:23 -0600

    fix github

--- data/fcchnas3/backup.remote.github.sh
+++ data/fcchnas3/backup.remote.github.sh
@@ -5,4 +5,4 @@ cd -- "$(dirname -- "$0")"
 
 export GITHUB_USER=$(cat /etc/fcchbackup/github-user)
 export GITHUB_TOKEN=$(cat /etc/fcchbackup/github-token)
-nestedlog run-as-block "org fortcollinscreatorhub" ./github-backup-org.py fortcollinscreatorhub ../../git/github
+nestedlog run-as-block "org fortcollinscreatorhub" ../github/github-backup-org.py fortcollinscreatorhub ../../git/github

bb23858d80e3c55eaf5dc6ca9893bd161f09dd8b
Stephen Warren <swarren@wwwdotorg.org>
2026-09-19 14:00:14 -0600

    fcchfw1: configure syncoid

---
+++ data/fcchfw1/syncoid.inc.sh
@@ -0,0 +1,2 @@
+syncs+=("fcchfw1_bpool fcchnas3 root@10.1.10.146:fcchnas3_rpool/REMOTE/syncoid/fcchfw1/fcchfw1_bpool 22")
+syncs+=("fcchfw1_rpool fcchnas3 root@10.1.10.146:fcchnas3_rpool/REMOTE/syncoid/fcchfw1/fcchfw1_rpool 22")

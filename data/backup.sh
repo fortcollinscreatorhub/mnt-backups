a7d694535789a994da6c518d7470d545fdd024d2
Stephen Warren <swarren@wwwdotorg.org>
2026-09-22 17:25:40 -0600

    send log email to an extension address

--- data/backup.sh
+++ data/backup.sh
@@ -7,6 +7,6 @@ umask 077
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 exec nestedlog-email \
     -f "$(id -un)@$(hostname)" \
-    -t "sysadmin@fortcollinscreatorhub.org" \
+    -t "sysadmin+$(hostname)@fortcollinscreatorhub.org" \
     -s "FCCH $(hostname) nightly backups" \
     ./backup.logged.sh
